e5a762a80b8464512d9cf474300ffc010001ade6
Masayoshi Sekimura <sekimura@gmail.com>
2015-02-08 17:07:15 -0800

    first

---
+++ lookup/main.swift
@@ -0,0 +1,27 @@
+//
+//  main.swift
+//  lookup
+//
+//  Created by Masayoshi Sekimura on 2/8/15.
+//  Copyright (c) 2015 sekimura.org. All rights reserved.
+//
+
+import Foundation
+import CoreServices
+
+func main() {
+  let args = [String](Process.arguments)
+  
+  if args.count > 1 {
+    var word : String = args[1]
+    var range : CFRange =  CFRangeMake(0, (word as NSString).length)
+    var result : String? = DCSCopyTextDefinition(nil, word, range)?.takeRetainedValue()
+    if result == nil {
+      println("No entries found")
+      return
+    }
+    println(result!)
+  }
+}
+
+main()
